6dc0ec2efc874834148a9f898dcb3a0496ef2690
SheikhNoor <mdnurullah.co@gmail.com>
2025-02-24 13:13:05 +0530

    update

--- contracts/Mapping/nestedMapping.sol
+++ contracts/Mapping/nestedMapping.sol
@@ -1,18 +1,2 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.5.0 < 0.9.0;
-
-contract nestedMapping{
-     mapping(address=>mapping (address=> bool)) public ownership;
-
-     function provideOwnership(address sender, address reciever) public{
-        ownership[sender][reciever] = true;
-     }
-
-    function takeOwnership(address sender, address reciever) public{
-        ownership[sender][reciever] = false;
-     }
-
-    function checkOwnership(address sender, address reciever) public view returns(bool){
-        return ownership[sender][reciever];
-     }
-}
